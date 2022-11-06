62ac3588221a2ef7a3445d361c5b56e4c14a7eaa
praveena0402 <priyapraveena4@gmail.com>
2022-11-07 00:11:32 +0530

    Fist time push

---
+++ src/main/java/GitHub10am/GitHub10am/App.java
@@ -0,0 +1,13 @@
+package GitHub10am.GitHub10am;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/GitHub10am/GitHub10am/Ajava.java
@@ -0,0 +1,5 @@
+package GitHub10am.GitHub10am;
+
+public class Ajava {
+
+}
