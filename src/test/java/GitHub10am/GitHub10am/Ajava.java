eb11032963cee1f91c47edb6ffa2c6603bd2c61f
praveena0402 <priyapraveena4@gmail.com>
2022-11-07 10:47:06 +0530

    First time push

--- src/test/java/GitHub10am/GitHub10am/Ajava.java
+++ src/test/java/GitHub10am/GitHub10am/Ajava.java
@@ -1,5 +1,18 @@
 package GitHub10am.GitHub10am;
 
 public class Ajava {
+	public void method1() {
+		System.out.println("1");
+		
+	}
+	
+	public void method2() {
+		System.out.println("2");
+	}
+	
+	public void method3() {
+		System.out.println("3");
+		
+	}
 
 }
